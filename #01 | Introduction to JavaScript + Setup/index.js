c7687dbdc78d2f24237f43932bd4a6c9b637d6c0
pseudovicky <sudovicky@gmail.com>
2023-08-12 11:47:52 +0530

    first Js program

--- #01 | Introduction to JavaScript + Setup/index.js
+++ #01 | Introduction to JavaScript + Setup/index.js
@@ -1,1 +1,8 @@
 console.log("Hello, World!")
+
+// write a Program to add three number
+function addThreeNumbers(a,b,c) {
+    return a + b + c
+  }
+  let c = addThreeNumbers(1,2,7)
+  console.log(c)
