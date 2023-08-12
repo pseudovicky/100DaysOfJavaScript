e98d7b532cd349afd78504e065ec3fcb7c33f2a0
pseudovicky <sudovicky@gmail.com>
2023-08-12 12:54:23 +0530

    Variable and Data Types

---
+++ #02 | Variables in JavaScript/index.js
@@ -0,0 +1,14 @@
+// Variables and DataTypes
+
+console.log("The Unlimate javaScript course Day 2 Understanding Variable and DataTypes in JS.")
+
+let a = 100 // a contains 100
+console.log(a)
+
+a = "Vicky"
+console.log(a)  // runtime changed
+
+var n = 200
+console.log(n)
+n = "JavaScript"
+console.log(n)
