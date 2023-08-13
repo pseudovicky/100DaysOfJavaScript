479c21f887b274ef69b4226b2c1cfcf540b9b491
pseudovicky <sudovicky@gmail.com>
2023-08-13 16:41:44 +0530

    question 2

--- #05 | JavaScript Chapter 1 -Practice Set/index.js
+++ #05 | JavaScript Chapter 1 -Practice Set/index.js
@@ -5,3 +5,10 @@ let a = "Vicky"
 let b = 6
 console.log(a + b)
 
+// 2. Use typeof operator to find the datatype of the string in last question.
+data_type = typeof a
+console.log(data_type)
+data_type2 = typeof b
+console.log(data_type2)
+console.log(typeof (a+b) )
+
