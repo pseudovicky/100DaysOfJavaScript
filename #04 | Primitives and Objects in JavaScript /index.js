3a528c92c68fcdca205dd66ef89fe289e7df5ede
pseudovicky <sudovicky@gmail.com>
2023-08-13 14:49:07 +0530

    Primitives And Non Primitive(object)   Data Type

---
+++ #04 | Primitives and Objects in JavaScript /index.js
@@ -0,0 +1,50 @@
+// Types of Primitives Data Types
+
+// NULL
+// NUMBER
+// SYMBOL
+// STRING
+// BOOLEAN
+// BIGINT
+// UNDEFINED
+
+
+// Note:-  NONE PRIMITIVE DATA TYPE IS OBJECT IN JAVASCRIPT
+
+// nn bb ss u - primitives in js
+
+let a = null;
+let b = 345;
+let c = true;
+let d = BigInt("567") + BigInt("3")
+let e = "Vicky"
+let f = Symbol("I am a nice Symbol")
+let g = undefined
+let h
+
+console.log(a,b,c,d,e,f,g,h)
+
+console.log( typeof a)
+console.log( typeof b)
+console.log( typeof c)
+console.log( typeof d)
+console.log( typeof e)
+console.log( typeof f)
+console.log( typeof g)
+console.log( typeof h)
+
+
+// Non Primitives Data Type - Objects in JS
+
+const item = {
+  "Vicky" : true,
+  "Jaldi_waha_se_hato": false,
+  "Aree_bhai_bhai_bhai_bhai": 67,
+  "Mao" : undefined,
+}
+
+console.log(item["Vicky"])
+console.log(item["Jaldi_waha_se_hato"])
+console.log(item["hue"])
+console.log(item["Mao"])
+
