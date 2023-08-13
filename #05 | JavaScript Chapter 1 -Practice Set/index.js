c176454e2235f2458c5ab90703ac147b0bf61d16
pseudovicky <sudovicky@gmail.com>
2023-08-13 16:42:05 +0530

    question 3

--- #05 | JavaScript Chapter 1 -Practice Set/index.js
+++ #05 | JavaScript Chapter 1 -Practice Set/index.js
@@ -12,3 +12,13 @@ data_type2 = typeof b
 console.log(data_type2)
 console.log(typeof (a+b) )
 
+// 3. Create a const object in javascript can you change it to hold a number later ?
+const p = {
+    name : "Vicky",
+    section : 1,
+    isPrinciple: false
+  }
+  // p = "Viki" 
+  // No, We can not change the value of const.
+  
+  
