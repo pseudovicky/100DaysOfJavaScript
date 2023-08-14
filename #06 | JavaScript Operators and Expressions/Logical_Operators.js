ccf0122d912e1b651a96c063295cc8d0f3c1d4b6
pseudovicky <sudovicky@gmail.com>
2023-08-14 17:35:12 +0530

    logical operators

---
+++ #06 | JavaScript Operators and Expressions/Logical_Operators.js
@@ -0,0 +1,17 @@
+// Logical Operators:- 
+
+// && - logical and
+
+let x = 5;
+let y = 6;
+console.log(x<y && x==5)
+console.log(x<y && x==y)
+
+// || logical or
+console.log(x<y || x!=5)
+console.log(x>y || x>=y)
+
+// ! logical not
+
+console.log(!false)
+console.log(!true)
