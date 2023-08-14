ebbfdf302922a33e8997a471cf48b098424fb005
pseudovicky <sudovicky@gmail.com>
2023-08-15 01:11:55 +0530

    solution 1

--- #08 | JavaScript Chapter 2 - Practice Set on Operators and Conditionals /index.js
+++ #08 | JavaScript Chapter 2 - Practice Set on Operators and Conditionals /index.js
@@ -2,7 +2,7 @@
 
 /* 
 1.  Use Logical Operators to find whether the age of the person lies 
-    between 10 and 29. 
+    between 10 and 20. 
 2. Demonstrate the use of switch case statements in javascript.
 3. Write a javascript program to find whether a number is divisible 
     by 2 and 3.
@@ -12,3 +12,10 @@
     greater than 18 using ternary operator.
 */
 
+// Solution 1 :-
+let age = parseInt(prompt("Enter your age: "))
+if(age>10 && age<20){
+    console.log("Your Age is lies between 10 and 20.")
+}else{
+    console.log("Your Age doesn't lies between 10 and 20.")
+}
