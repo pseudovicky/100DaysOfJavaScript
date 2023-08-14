091b7c4b76547598bd91fa9711ab675b6f264a8e
pseudovicky <sudovicky@gmail.com>
2023-08-15 01:03:55 +0530

    questions practice set 2

---
+++ #08 | JavaScript Chapter 2 - Practice Set on Operators and Conditionals /index.js
@@ -0,0 +1,14 @@
+// chapter 2 - Practice Sets :-
+
+/* 
+1.  Use Logical Operators to find whether the age of the person lies 
+    between 10 and 29. 
+2. Demonstrate the use of switch case statements in javascript.
+3. Write a javascript program to find whether a number is divisible 
+    by 2 and 3.
+4. Write a javascript program to find whether a number is divisible
+    by either 2 or 3.
+5. Print "Drive" or "You can not Drive" based on age being 
+    greater than 18 using ternary operator.
+*/
+
