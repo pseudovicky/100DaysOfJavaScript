59ea01469838f98e2871c8befd546e2cb41bf9dd
pseudovicky <sudovicky@gmail.com>
2023-08-15 01:52:12 +0530

    problem 4

---
+++ #08 | JavaScript Chapter 2 - Practice Set on Operators and Conditionals /problem4.js
@@ -0,0 +1,7 @@
+// problem 4.
+let num = parseInt(prompt("Enter number: "))
+if (num%2==0 || num%3==0){
+  console.log(num," is divisible by either 2 or 3.");
+}else{
+  console.log(num, " is not divisible by either 2 and 3.")
+}
