0abb7e929de196592ea60839f39ab14a149242ad
pseudovicky <sudovicky@gmail.com>
2023-08-14 17:23:05 +0530

    arithematic operators

---
+++ #06 | JavaScript Operators and Expressions/arithematic_Opeartors.js
@@ -0,0 +1,21 @@
+77;
+"Vicky kumar"
+false
+
+
+console.log("opeartors in JS")
+// Arithematic operators 
+
+let a = 45;
+let b = 5;
+result = a + b;
+console.log("a + b = ", result)
+console.log("a - b = " , a-b)
+console.log("a * b = ", a*b)
+console.log("2 ** 3 = " ,2**3)
+console.log("a / b = ", a/b)
+console.log("99 % 10 = ", 99%10)
+console.log(a,a++,a++,a++)
+console.log(a,a--,a--,a--)
+console.log(a,--a,--a,--a)
+console.log(a,++a,++a,++a)
