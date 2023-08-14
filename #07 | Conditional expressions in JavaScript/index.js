4cd7edb3791ef434c047f3c5579274ce1b4fbe58
pseudovicky <sudovicky@gmail.com>
2023-08-14 20:42:18 +0530

    if else and typecasting

---
+++ #07 | Conditional expressions in JavaScript/index.js
@@ -0,0 +1,16 @@
+let age = prompt("Hey what's your age? ")
+
+console.log(age, typeof age)
+
+alert("This is a alert!")
+
+// prompt takes input alert show output
+
+age = Number.parseInt(age)
+console.log(age, typeof age)
+// If else conditional statement :-
+if(age>=18){
+  alert("This is a valid age for Driving.")
+}else{
+  alert("Abhi tu thora bada ho jaa!")
+}
