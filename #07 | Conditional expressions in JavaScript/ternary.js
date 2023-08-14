d3fb96efa3961e812143f498e5e69bd8a525b75d
pseudovicky <sudovicky@gmail.com>
2023-08-14 22:31:46 +0530

    ternary operators

---
+++ #07 | Conditional expressions in JavaScript/ternary.js
@@ -0,0 +1,5 @@
+// Ternary Operators :- 
+
+let marks = parseInt(prompt("Input your marks here : "))
+console.log(marks, typeof marks)
+console.log(" Your marks is", marks , " and you are", (marks>=40) ? "Pass." : "Fail.")
