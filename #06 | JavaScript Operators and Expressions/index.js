d01158632150085a86fd470335e00b4b60b4e4da
pseudovicky <sudovicky@gmail.com>
2023-08-14 17:21:18 +0530

    comparison operators

--- #06 | JavaScript Operators and Expressions/index.js
+++ #06 | JavaScript Operators and Expressions/index.js
@@ -60,3 +60,41 @@ l = 2
 console.log(l)
 l **= 3
 console.log(l)
+
+
+// Comparison Operators:- 
+
+// ==
+let comp1 = 6;
+let comp2 = 7;
+console.log("comp1 == comp2",comp1 == comp2)
+
+// !=
+console.log("comp1 != comp2",comp1 != comp2)
+
+// ===
+console.log("comp1 === comp2",comp1 === comp2)
+const q = 10;
+const t = 10;
+console.log("Type of q = ",typeof q)
+console.log("Type of t = ",typeof t)
+console.log("q === t",q === t)
+// this operator Compare type and value
+
+// !==
+console.log("comp1 != comp2",comp1 !== comp2)
+const c = 25;
+let d = 25;
+console.log("Type of c = ",typeof c)
+console.log("Type of d = ",typeof d)
+console.log("c !== d",c !== d)
+
+// <= & >=
+console.log("10 > 20",10>20)
+console.log("10 < 20",10<20)
+console.log("10 >= 20", 10>=20)
+console.log("10 <= 20", 10<=20)
+
+
+// ternary operator  ?
+// skip for now 
