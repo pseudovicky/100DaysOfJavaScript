93d3549162eb7afe86eb8db796cfe728046d58ed
pseudovicky <sudovicky@gmail.com>
2023-08-14 17:23:55 +0530

    assignment operators

---
+++ #06 | JavaScript Operators and Expressions/Assignment_Operators.js
@@ -0,0 +1,39 @@
+
+//  Assignment Operators :- 
+
+// =
+x = 10;
+console.log(x)
+y = x
+console.log(y)
+
+// +=
+z = 20
+console.log(z)
+z += 20
+console.log(z)
+
+// -=
+n = 20
+console.log(n)
+n -= 5
+console.log(n)
+
+// /=
+s = 20
+console.log(s)
+s /= 5
+console.log(s)
+
+// %=
+p = 27
+console.log(p)
+p %= 5
+console.log(p)
+
+// **=
+l = 2
+console.log(l)
+l **= 3
+console.log(l)
+
