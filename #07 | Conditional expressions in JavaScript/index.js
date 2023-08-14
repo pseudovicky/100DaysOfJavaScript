7e77c1f95e0284ab490be5f8d107098abfee05dc
pseudovicky <sudovicky@gmail.com>
2023-08-14 20:44:05 +0530

    type casting

--- #07 | Conditional expressions in JavaScript/index.js
+++ #07 | Conditional expressions in JavaScript/index.js
@@ -6,11 +6,9 @@ alert("This is a alert!")
 
 // prompt takes input alert show output
 
+// Typecasting
 age = Number.parseInt(age)
 console.log(age, typeof age)
-// If else conditional statement :-
-if(age>=18){
-  alert("This is a valid age for Driving.")
-}else{
-  alert("Abhi tu thora bada ho jaa!")
+
+
 }
