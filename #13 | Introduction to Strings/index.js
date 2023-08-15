ee78cbb0886b7221277d248165a04453f1819d85
pseudovicky <sudovicky@gmail.com>
2023-08-16 01:05:28 +0530

    string manipulation

--- #13 | Introduction to Strings/index.js
+++ #13 | Introduction to Strings/index.js
@@ -2,3 +2,28 @@ let name = "vicky"
 
 console.log(name)
 console.log(name.length)
+
+console.log(name)
+console.log(name.length)
+
+let message = 'Jaldi waha se hato'
+console.log(message,message.length)
+
+let language = `Javascript`
+console.log(language,language.length)
+console.log(language[0])
+console.log(language[1])
+console.log(language[2])
+console.log(language[3])
+console.log(language[4])
+console.log(language[5])
+console.log(language[6])
+console.log(language[7])
+console.log(language[8])
+console.log(language[9])
+// console.log(language[10])
+console.log(language[language.length-1])
+
+// indexing starts from 0 and length starts from 1.
+
+
