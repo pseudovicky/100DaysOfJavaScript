64df0d5c5c4d87c15add22c207bfe389d2fe8f42
pseudovicky <sudovicky@gmail.com>
2023-08-15 12:42:57 +0530

    var scope and let scope

---
+++ #09 | For Loops in JavaScript/scope.js
@@ -0,0 +1,29 @@
+// // let scope
+
+// let marks = {
+//     vicky : 90,
+//     shiv : 90,
+//     ritika : 50,
+//     ramuKaka : 40,
+//     vasuliBhai : 23,
+//   }
+  
+//   for( let i in marks){
+//     console.log("Marks of " + i + " are " + marks[i])
+//   }
+//   console.log(i)
+
+// Var scope
+
+let marks = {
+    vicky : 90,
+    shiv : 90,
+    ritika : 50,
+    ramuKaka : 40,
+    vasuliBhai : 23,
+  }
+  
+  for( var i in marks){
+    console.log("Marks of " + i + " are " + marks[i])
+  }
+  console.log(i)
