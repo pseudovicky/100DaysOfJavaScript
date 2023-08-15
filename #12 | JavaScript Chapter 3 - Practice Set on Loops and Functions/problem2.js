4967ed8cde00ce99e98bbd7563245947ae62ddf5
pseudovicky <sudovicky@gmail.com>
2023-08-15 16:01:04 +0530

    problem 2 solution

---
+++ #12 | JavaScript Chapter 3 - Practice Set on Loops and Functions/problem2.js
@@ -0,0 +1,14 @@
+// 2. Write thr program in Q1 using for in loop.
+
+// problem 2. solution :-
+
+let marks = {
+    vicky : 98,
+    rohan : 70,
+    aakash: 7,
+  monika: 50,
+}
+
+for (let i in marks){
+  console.log(marks[i])
+}
