7130ee2ae7327cdd0da56a9d82997127d921c292
pseudovicky <sudovicky@gmail.com>
2023-08-15 19:38:23 +0530

    problem 3 solution

---
+++ #12 | JavaScript Chapter 3 - Practice Set on Loops and Functions/problem3.js
@@ -0,0 +1,11 @@
+// 3. Write a program to print " try again" untill the user enters the correct number.
+
+//  problem 3 solution:- 
+
+let num = 4
+let i
+while(i != num){
+  console.log("Try Again")
+  i = prompt("Enert a number : ")
+}
+console.log("You have entered a correct number")
