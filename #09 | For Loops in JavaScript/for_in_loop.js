583777ad07b2a7653fa041425b5cafc82947aa12
pseudovicky <sudovicky@gmail.com>
2023-08-15 12:33:57 +0530

    for in loop

---
+++ #09 | For Loops in JavaScript/for_in_loop.js
@@ -0,0 +1,13 @@
+// for in loop 
+
+let marks = {
+    vicky : 90,
+    shiv : 90,
+    ritika : 50,
+    ramuKaka : 40,
+    vasuliBhai : 23,
+  }
+  
+  for( let i in marks){
+    console.log("Marks of " + i + " are " + marks[i])
+  }
