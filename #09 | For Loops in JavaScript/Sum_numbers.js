2475c7aef02ed99ae910bf6ac76a443fe8731c17
pseudovicky <sudovicky@gmail.com>
2023-08-15 12:27:07 +0530

    for loop program

--- #09 | For Loops in JavaScript/Sum_numbers.js
+++ #09 | For Loops in JavaScript/Sum_numbers.js
@@ -1,6 +1,7 @@
 // Program to add first n natural numbers
 let sum = 0
-for(let i =0; i< 30; i++){
+let n = parseInt(prompt("Enter the number : "))
+for(let i =0; i< n; i++){
   sum += i
 }
-console.log("sum = " ,sum)
+console.log("sum of 1 to = ",n,"is = " ,sum)
