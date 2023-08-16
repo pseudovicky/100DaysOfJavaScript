9c4362511180e64b419ed68c8b9d46d61c2580be
pseudovicky <sudovicky@gmail.com>
2023-08-16 12:19:00 +0530

    template literals

--- #13 | Introduction to Strings/index.js
+++ #13 | Introduction to Strings/index.js
@@ -24,6 +24,15 @@ console.log(language[9])
 // console.log(language[10])
 console.log(language[language.length-1])
 
-// indexing starts from 0 and length starts from 1.
+// note - indexing starts from 0 and length starts from 1.
 
 
+// Template literals 
+
+let boy1 = "Vicky"
+let boy2 = "Ritesh"
+// Ritesh is a friend of vicky
+
+// let sentance = `boy2 is a friend of boy1`
+let sentance = `${boy2} is friend of ${boy1}`
+console.log(sentance)
