ed0072b2241b38125901206fd08d53ed96930212
pseudovicky <sudovicky@gmail.com>
2023-08-16 15:49:45 +0530

    solution 1

---
+++ #15 | JavaScript Chapter 4 - Practice Set on Strings/index.js
@@ -0,0 +1,22 @@
+// Chapter 4 - practice set 
+
+/*
+1. What will the following print in javascript?
+console.log("har\"".length)
+
+2. Explore the includes, starts with  endswith functions of a string.
+
+3. Write a program to convert a given string to lowercase.
+
+4. Extract the amount out of this string.
+" Please give Rs 1000"
+
+5. Try to change 4th characters of a given string were you able to do ot?
+*/
+
+// problem 1. solution
+// answer is =  4
+console.log("har\"".length)
+
+// problem 2 solution 
+
