ba2c9733ef07b00ad652b169f202e8bf45e99a35
pseudovicky <sudovicky@gmail.com>
2023-08-16 16:44:07 +0530

    arrays in js

---
+++ #16 | Introduction to Arrays/index.js
@@ -0,0 +1,9 @@
+let a = [1,2,3,4,5,6,7]
+console.log(a, typeof a) // type object
+console.log(a[4]) //indexing
+
+// This is an Array.
+
+let marks_class_12 = [91, 82, 63, 89, 56, 74]
+console.log(marks_class_12)
+
