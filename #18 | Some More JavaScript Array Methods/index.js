48db56086497a8088de0ab3bc7c40d9b7ad48474
pseudovicky <sudovicky@gmail.com>
2023-08-16 19:20:34 +0530

    slice and splice

--- #18 | Some More JavaScript Array Methods/index.js
+++ #18 | Some More JavaScript Array Methods/index.js
@@ -17,26 +17,47 @@ console.log(z, typeof z)
 
 // alphabetically sorted as string
 
-// let mn = [1,8,90,87,54,43,5,50,9]
-// console.log(mn)
-// mn.sort()
-// console.log(mn)
+let mn = [1,8,90,87,54,43,5,50,9]
+console.log(mn)
+mn.sort()
+console.log(mn)
 
 
 // ascending order sorting
-// let compare = (a,b) =>{
-//   return a-b
-// }
-// let mn = [1,8,90,87,54,43,5,50,9]
-// console.log(mn)
-// mn.sort(compare)
-// console.log(mn)
+let compare = (a,b) =>{
+  return a-b
+}
+let dn = [1,8,90,87,54,43,5,50,9]
+console.log(dn)
+dn.sort(compare)
+console.log(dn)
 
 // descending order sorting
-let compare = (a,b) =>{
-  return b-a
-}
-let mn = [1,8,90,87,54,43,5,50,9]
-console.log(mn)
-mn.sort(compare)
-console.log(mn)
+// let compare = (a,b) =>{
+//   return b-a
+// }
+let pn = [1,8,90,87,54,43,5,50,9]
+console.log(pn)
+pn.sort(compare)
+console.log(pn)
+
+let d = [9,5,87,34,2,45]
+console.log(d)
+d.reverse()
+console.log(d)
+
+// Splice 
+
+let nums = [551, 22, 3, 14, 5, 6, 7, 8, 229]
+let deletedValues = nums.splice(2,3,1021,1022,1023,1024,34578)
+console.log(nums)
+console.log(deletedValues, typeof deletedValues)
+
+// slice
+
+let a = [10,20,30,40,100,80,30]
+g = a.slice(3) // index 3 to end
+console.log(g)
+k = a.slice(1,4) // index 1 to 4
+console.log(k)
+console.log(a)
