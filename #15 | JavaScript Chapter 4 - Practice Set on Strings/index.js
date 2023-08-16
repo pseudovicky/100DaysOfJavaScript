e28b00fafa7a837b45a71bd5ba8f7c06a1c0c91c
pseudovicky <sudovicky@gmail.com>
2023-08-16 16:09:44 +0530

    startsWith and endsWith, problem 2

--- #15 | JavaScript Chapter 4 - Practice Set on Strings/index.js
+++ #15 | JavaScript Chapter 4 - Practice Set on Strings/index.js
@@ -18,5 +18,18 @@ console.log("har\"".length)
 // answer is =  4
 console.log("har\"".length)
 
+
 // problem 2 solution 
+// startsWith and endsWith
+const sentence = 'The quick brown fox jumps over the lazy dog.';
+const word = 'fox2';
+console.log(`The word "${word}" ${sentence.includes(word) ? 'is' : 'is not'} in the sentence`);
+
+// include returns true or false .
+
+let msg = "Good Morning!"
+console.log(msg.startsWith("G") ? "Sahi baat hai" : "No its wrong")
+console.log(msg.endsWith("ing!") ? "Sahi baat hai" : "No its wrong")
+console.log(msg.startsWith("Good",0) ? "Sahi baat hai" : "No its wrong")
+console.log(msg.endsWith("ing!",13) ? "Sahi baat hai" : "No its wrong")
 
