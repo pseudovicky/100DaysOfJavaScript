1ef2a23447c17ab043958307054d270185e44e9e
pseudovicky <sudovicky@gmail.com>
2023-08-17 15:40:01 +0530

    challenge number guessing game

---
+++ #22 | Exercise 1 - Guess the Number/index.js
@@ -0,0 +1,11 @@
+/* 
+Exercise 1 - Guess The Number:-
+
+Write a js program to genrate a random number and store it in a random variable. 
+The program then taken an input from the user to tell them whether the guess was correct, 
+greater or lower than the original number.
+100 - (no of guesses) is the score of the user. 
+The program is expected to terminate once the number is guessed, 
+Number should be between 1-100.
+*/
+
