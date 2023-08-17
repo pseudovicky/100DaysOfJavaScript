c1a65d07afadb4eabc4185d170bacc84c8dc8382
pseudovicky <sudovicky@gmail.com>
2023-08-17 11:36:51 +0530

    problem 2 solution

--- #21 | JavaScript Chapter 5 - Practice Set on Arrays/index.js
+++ #21 | JavaScript Chapter 5 - Practice Set on Arrays/index.js
@@ -20,3 +20,18 @@ for(let i=0;i<n;i++){
   array1.push(num)
 }
 console.log("This is your final array = ", array1)
+
+
+// problem 2 solution 
+
+array2 = []
+keepadding = true
+while(keepadding){
+  additem = prompt("add item value: ")
+  if(additem!=0){
+    array2.push(additem)
+  }else{
+    keepadding = false
+  }  
+}
+console.log(array2)
