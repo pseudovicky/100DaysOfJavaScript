70ae97ef4e9eb76c63decc8cae8336b7333139d6
pseudovicky <sudovicky@gmail.com>
2023-08-17 11:27:42 +0530

    solution 1

---
+++ #21 | JavaScript Chapter 5 - Practice Set on Arrays/index.js
@@ -0,0 +1,22 @@
+// Chapter 5 - Practice set
+/*
+1. Create an array of numbers and take input from the user to add numbers to this array.
+
+2. keep adding numbers to the array in (1) until 0 is added to the array.
+
+3. filter for numbers divissible by 10 from a given array.
+
+4. create an array of square of given numbers
+
+5. Use reduce to calculate factorial of a given numbers from an array of first ( n natural numbers where factorial needs to be calculated. )
+*/
+
+// problem 1 solution:
+
+array1 = []
+n = prompt("How many numbers do you want to add an array: ")
+for(let i=0;i<n;i++){
+  num = prompt("Enter value: ")
+  array1.push(num)
+}
+console.log("This is your final array = ", array1)
