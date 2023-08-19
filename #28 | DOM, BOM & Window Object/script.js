d56f993bc59d01e007b59092957c0d5c07798fab
pseudovicky <sudovicky@gmail.com>
2023-08-19 15:21:09 +0530

    DOM and BOM in js

---
+++ #28 | DOM, BOM & Window Object/script.js
@@ -0,0 +1,11 @@
+// console.log(window)
+window.console.log(window)
+// apne pure hHTML page ko document variable me assign kiya gya hai in JavaScript.
+console.log(document.body)
+document.body.style.background = "yellow"
+
+
+
+// BOM
+// alert("vicky)
+// location.href = "https:google.com"
