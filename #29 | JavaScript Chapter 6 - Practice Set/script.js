cb79569e092664de68bf2a02e9cbfedf32625fd1
pseudovicky <sudovicky@gmail.com>
2023-08-19 17:40:06 +0530

    day 29 chapter solution

--- #29 | JavaScript Chapter 6 - Practice Set/script.js
+++ #29 | JavaScript Chapter 6 - Practice Set/script.js
@@ -1,7 +1,8 @@
 document.body.style.backgroundColor = "yellow"
 
-let seeAgain = confirm("Do you want to See the prompt again!")
-if(seeAgain){
+let seeAgain = true
+
+while(seeAgain){
 age = parseInt(prompt("Enter your age: "))
 if(age>=18){
   console.log("You can Drive!")
@@ -9,10 +10,11 @@ if(age>=18){
 }else if(age<=0){
   console.error("Invalid age!")
   document.style.color = "red"
-}else if(age>4){
+}else if(age<4){
   location.href = "www.google.com"
 }else{
   console.error("You can not Drive!")
   document.style.color = "red"
 }
+  seeAgain = confirm("Do you want to See the prompt again!")
 }
