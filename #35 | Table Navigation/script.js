eb0f05affa539e30773035fac6a24caaa42ab0af
pseudovicky <sudovicky@gmail.com>
2023-08-27 21:33:10 +0530

    Table navigation in JS

---
+++ #35 | Table Navigation/script.js
@@ -0,0 +1,11 @@
+let t = document.body.firstElementChild.firstElementChild
+console.log(t)
+console.log(t.rows)
+console.log(t.caption)
+console.log(t.tHead.firstElementChild)
+console.log(t.tFoot)
+console.log(t.Bodies)
+console.log(t.rows[0].rowIndex)
+console.log(t.rows[1].rowIndex)
+console.log(typeof window)        //object
+console.log(typeof document)      //object
