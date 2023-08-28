aceaf83fcdfc64c3cfcd3eecb2222a77befe7e49
pseudovicky <sudovicky@gmail.com>
2023-08-28 12:20:00 +0530

    Snake Water Gun

---
+++ #30 | Exercise 2 - Snake Water Gun/script.js
@@ -0,0 +1,10 @@
+/* 
+Exercise 2 -  Snake Water Gun
+
+Use js to create a game of snake water & Gun. 
+The game should ask you to enter S, W, or G. 
+The computer should be able to randomly generate S, W or G and
+declare win or loss using alert use comfirm and prompt wherever required.
+
+*/
+
