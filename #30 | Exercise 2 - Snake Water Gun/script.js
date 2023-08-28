ef66710b653f1e13a7b6b2ee7394bfe900ad6bed
pseudovicky <sudovicky@gmail.com>
2023-08-28 12:43:49 +0530

    Snake water Gun game

--- #30 | Exercise 2 - Snake Water Gun/script.js
+++ #30 | Exercise 2 - Snake Water Gun/script.js
@@ -8,3 +8,27 @@ declare win or loss using alert use comfirm and prompt wherever required.
 
 */
 
+let user = prompt("Enter S, W or G : ")
+let cpuI = math.floor(math.random() * 3)
+let cpu = ["S", "W","G"][cpuI]
+
+const match = (cpu, user)=>{
+  if (cpu == user){
+    return "Nobody. match is tied."
+  }else if(cpu == "S" && user == "W"){
+    return "cpu"
+  }else if(cpu == "S" && user == "G"){
+    return "user"
+  }
+  else if(cpu == "G" && user == "W"){
+    return "user"
+  }else if(cpu == "G" && user == "S"){
+    return "cpu"
+  }else if(cpu == "W" && user == "S"){
+    return "user"
+  }else if(cpu == "w" && user == "G"){
+    return "cpu"
+  }
+}
+let result = match(cpu, user)
+console.log("The winner is : " + result)
