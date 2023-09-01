0af5f873801f95241b5eb73a73a46ed7a04760f9
pseudovicky <sudovicky@gmail.com>
2023-09-01 15:57:45 +0530

    js chapter 8 - practice set

---
+++ #49 | JavaScript Chapter 8 - Practice Set/script.js
@@ -0,0 +1,13 @@
+// using event listener
+
+document.getElementById("google").addEventListener("click", function(){
+    window.location = "https://www.google.com";
+})
+
+document.getElementById("fb").addEventListener("click", function(){
+    window.location = "https://www.facebook.com";
+})
+
+document.getElementById("twitter").addEventListener("click", function(){
+    window.location = "https://www.twitter.com";
+})
