e94ad2e7de0e1883a22fe1c4571000710010a418
pseudovicky <sudovicky@gmail.com>
2023-09-04 22:19:42 +0530

    reject

--- #56 | Promise .then() and .catch()/script.js
+++ #56 | Promise .then() and .catch()/script.js
@@ -15,3 +15,20 @@ p1.then((value)=>{
 
 
 
+// reject 
+let p2 = new Promise((resolve, reject) => {
+    console.log('promise is pending')
+    setTimeout(() =>{
+        // console.log('I am a promise and i am rejected')
+        reject(new Error('I am an error'))
+    }, 5000)
+})
+
+// console.log(p2)
+// p2.then((value)=>{
+//     console.log(value)
+// })
+
+p2.catch((error)=>{
+    console.log('Some error occurred in p2.')
+})
