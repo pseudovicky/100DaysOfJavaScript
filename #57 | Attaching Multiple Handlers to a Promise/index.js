a1ee6cffeeb0780757b94974297eb85c9d6d514e
pseudovicky <sudovicky@gmail.com>
2023-10-29 12:33:30 +0530

    Attaching multiple headers to a promise - Day 57

--- #57 | Attaching Multiple Handlers to a Promise/index.js
+++ #57 | Attaching Multiple Handlers to a Promise/index.js
@@ -5,3 +5,16 @@ let p1 = new Promise((resolve, reject)=>{
     }, 2000)
 })
 
+p1.then(()=>{
+    console.log("Hurray")
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            return 4
+        }, 6000)
+    })
+}).then((value) => { console.log(value) })
+
+p1.then(()=> {
+    // alert("Hurray")
+    console.log("Congratulation this promise is now resolved")
+})
