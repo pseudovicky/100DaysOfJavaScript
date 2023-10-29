c3ad8989394403df228ce04d161cde6c886f5956
pseudovicky <sudovicky@gmail.com>
2023-10-29 12:21:14 +0530

    js 56

--- #56 | Promise Chaining .then() calls/script.js
+++ #56 | Promise Chaining .then() calls/script.js
@@ -18,3 +18,19 @@ p1.then((value)=>{
 }).then((value)=>{
     console.log("Now we are pakka done")
 })
+
+const loadScript = (src)=>{
+    return new Promise((resolve, reject) => {
+    let script = document.createElement("script")
+    script.type = "text/javascript"
+    script.type = "text/javascript"
+    script.src = src
+    document.body.appendChild(script)
+    script.onload = ()=>{
+        resolve(1)
+    }
+        script.onerror = ()=>{reject(0)}
+    })
+}
+
+
