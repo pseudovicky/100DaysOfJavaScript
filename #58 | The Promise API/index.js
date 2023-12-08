3cf4a484214edbd13b272d0eb970d79bc705c0c8
pseudovicky <sudovicky@gmail.com>
2023-12-08 20:26:48 +0530

    promise API

--- #58 | The Promise API/index.js
+++ #58 | The Promise API/index.js
@@ -6,7 +6,8 @@ let p1 = new Promise((resolve, reject)=>{
 
 let p2 = new Promise((resolve, reject)=>{
     setTimeout(()=>{
-        resolve("value 2");
+        // resolve("value 2");
+        reject(new Error("Error"));
     }, 2000);
 });
 
@@ -16,19 +17,24 @@ let p3 = new Promise((resolve, reject)=>{
     }, 3000);
 });
 
-p1.then((value)=>{
-    console.log(value);
-})
+// p1.then((value)=>{
+//     console.log(value);
+// })
 
-p2.then((value)=>{
-    console.log(value);
-})
+// p2.then((value)=>{
+//     console.log(value);
+// })
 
-p3.then((value)=>{
-    console.log(value);
-})
+// p3.then((value)=>{
+//     console.log(value);
+// })
 
-let Promise_all = Promise.all([p1, p2, p3])
-Promise_all.then((value)=>{
+
+// let Promise_all = Promise.all([p1, p2, p3])
+// let promise_all = Promise.allSettled([p1, p2, p3])
+// let promise_all = Promise.race([p1, p2, p3])
+// let promise_all = Promise.resolve(6)
+let promise_all = Promise.reject(new Error("Hey"))
+promise_all.then((value)=>{
     console.log(value)
 })
