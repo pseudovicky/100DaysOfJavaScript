fc6fb739235395e632297d0f81341aee0382edcb
pseudovicky <sudovicky@gmail.com>
2023-12-19 00:05:17 +0530

    finally keyword

---
+++ #62 | The Finally Clause/Readme.md
@@ -0,0 +1,11 @@
+# The finally Clause
+
+The try...catch construct may have one more code clause : finally
+
+If it exists it runs in all cause :
+
+after try if there were no errors 
+after catch if there were errors.
+
+If there is a return in try, finally is executed just before the control returns to the outer code.
+
---
+++ #62 | The Finally Clause/index.js
@@ -0,0 +1,44 @@
+try {
+    let a = 0;
+    console.log('program ran successfully.');
+    console.log(Program);
+    console.log('program ran successfully.');
+} 
+
+catch(err){
+    console.log('There is an error.')
+    console.log(error-hu-main)
+}
+
+finally {
+    console.log("I am a good boy.")
+    console.log('This have super power to run always.');
+    // close the file
+    // close loop
+    // write to log file
+    // used for program clean up
+}
+
+
+
+// finally in return function 
+
+const f = () => {
+    try {
+        console.log('mao mao');
+        return 
+    }
+    catch (err){
+        console.log('nihao');
+        console.log('error aaya hai');
+    }
+    finally {
+        console.log('Mujhe naa Chuna pari hu main !');
+    }
+
+}
+
+f()
+console.log('END')
+
+
