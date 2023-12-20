d68d7f599ee778dbe6bdd5f4d81735b81c5b11ac
pseudovicky <sudovicky@gmail.com>
2023-12-20 15:46:36 +0530

    qs 3 solution

--- #63 | avaScript Chapter 9 - Practice Set/index.js
+++ #63 | avaScript Chapter 9 - Practice Set/index.js
@@ -58,35 +58,35 @@
 // Qs 3. Create a promise which rejects after 3 seconds. use an async / await to get its value. 
 //  use a try catch to handle its error.
 
-const loadScript = async(src)=>{
-    return new Promise((resolve, reject)=>{
-        let script = document.createElement("script");
-        script.src  = src
-        script.onload = ()=>{
-            resolve(src + " Done Success !");
-        }
-        document.head.append(script);
-    })
-}
+// const loadScript = async(src)=>{
+//     return new Promise((resolve, reject)=>{
+//         let script = document.createElement("script");
+//         script.src  = src
+//         script.onload = ()=>{
+//             resolve(src + " Done Success !");
+//         }
+//         document.head.append(script);
+//     })
+// }
 
-let p = ()  => {
-return new Promise((resolve, reject)=>{
-    setTimeout(()=>{
-        reject(new Error('Please this is not acceptable'));
-    }, 3000)
-  })
-}
+// let p = ()  => {
+// return new Promise((resolve, reject)=>{
+//     setTimeout(()=>{
+//         reject(new Error('Please this is not acceptable'));
+//     }, 3000)
+//   })
+// }
 
-let a = async () => {
-    try{
-        let c = await p()
-        console.log(c)
-    }
-    catch(err){
-        console.log('this error have been handled.');
-    }
-  }
-a();
+// let a = async () => {
+//     try{
+//         let c = await p()
+//         console.log(c)
+//     }
+//     catch(err){
+//         console.log('this error have been handled.');
+//     }
+//   }
+// a();
 
 // Promise 
 // {<pending>}[[Prototype]]: 
@@ -98,6 +98,50 @@ a();
 //  Qs 4. Write a program using promise. all() inside an async/await to await 3 promises. 
 //  compare its results with the case where we await these promises one by one.
 
+let p1 = async () =>{
+    return new Promise((resolve, reject) =>{
+        setTimeout(()=>{
+            resolve(10);
+        } ,2000)
+    })
+}
+
+let p2 = async () =>{
+    return new Promise((resolve, reject) =>{
+        setTimeout(()=>{
+            resolve(20);
+        } ,1000)
+    })
+}
+
+let p3 = async () =>{
+    return new Promise((resolve, reject) =>{
+        setTimeout(()=>{
+            resolve(30);
+        } ,3000)
+    })
+}
+
+const run = async () =>{
+    console.time('run')
+    let a1 = await p1()
+    console.log(a1);
+    let a2 = await p2()
+    console.log(a2);
+    let a3 = await p3()
+    console.log(a3);
+    console.timeEnd("run");
+}
+
+run();
 
 
+// Promise 
+// {<pending>}[[Prototype]]: 
+// Promise[[PromiseState]]: "fulfilled"
+// [[PromiseResult]]: undefined
 
+// 10
+// 20
+// 30
+// run: 6007.2080078125 ms
