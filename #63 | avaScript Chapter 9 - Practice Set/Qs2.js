82a89218941b75d8990b1543e287791a1bf44641
pseudovicky <sudovicky@gmail.com>
2023-12-20 15:26:29 +0530

    Qs 2 solution

--- #63 | avaScript Chapter 9 - Practice Set/Qs2.js
+++ #63 | avaScript Chapter 9 - Practice Set/Qs2.js
@@ -2,19 +2,19 @@
 
 // Qs 2. solution 
 
-const loadScript = async(src)=>{
-    return new Promise((resolve, reject)=>{
+const loadScript = async (src) => {
+    return new Promise((resolve, reject) => {
         let script = document.createElement("script");
-        script.src  = src
-        script.onload = ()=>{
+        script.src = src
+        script.onload = () => {
             resolve(src + " Done Success !");
         }
         document.head.append(script);
     })
 }
-const main2 = async ()=>{
+const main2 = async () => {
     console.log(new Date().getMilliseconds())
-let a = await loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js");
+    let a = await loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js");
     console.log(a);
     console.log(new Date().getMilliseconds());
 }
